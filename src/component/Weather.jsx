9217019c8c4c7f652173f0ca0d90e47a50cf1ee1
Chrismiweb <chrismibiteso@gmail.com>
2024-07-27 00:05:38 -0700

    weather map

---
+++ src/component/Weather.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react'
+
+function Weather() {
+    const[search, setSearch] = useState("")
+    const [city, setCity] = useState("")
+    const [lan,setLan] = useState("")
+
+    
+   async function fecthWeather(){
+        try {
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${search}&appid=16772b706cff27540f131e667573c244`)
+        .then((res)=>res.json())
+        if(response){
+            setCity(response)
+            setSearch(response)
+            setLan(response)
+            console.log(response)
+                
+        }
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }
+  return (
+    <div>
+        <input type="search" onChange={(e)=>setSearch(e.target.value)} value={search} placeholder='search location'/>
+        <button onClick={fecthWeather}>search</button>
+        
+        {
+            city &&
+            <p>{city[0].name}</p>
+
+        }      
+        {
+             lan &&
+             <p>{lan[0].lat}</p>
+        }  
+
+    </div>
+  )
+}
+
+export default Weather
